0fde624dbdb8f3838a6737d0732e6ff47bc0aaa2
Cheneba <chenebametuge14@gmail.com>
2025-10-01 11:46:20 +0100

    Initiated the node app and put starter code in the index.js, called express and mongoose

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose
+    .connect("mongodb://localhost:27017/cartwish")
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log("MongoDB Connection Failed"));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
