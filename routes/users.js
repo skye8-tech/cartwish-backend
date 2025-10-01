d5dd98b98f87955aa68b3c9845e4ba70fc759e6f
Cheneba <chenebametuge14@gmail.com>
2025-10-01 16:52:22 +0100

    Implemented backend validation with Joi

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
 const User = require("../models/users");
+
+const createUserSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+    address: Joi.string().min(5).required(),
+});
 
 router.post("/", async (req, res) => {
     const { name, email, password, address } = req.body;
+
+    // Validation with Joi
+    const joiValidation = createUserSchema.validate(req.body);
+    if (joiValidation.error) {
+        return res.status(400).json(joiValidation.error.details[0].message);
+    }
+
+    // Checks if user exists already
     const user = await User.findOne({ email: email });
 
     if (user) {
         return res.status(400).json({ message: "user already exists" });
     }
 
+    // Creates new user
     const hashedPass = await bcrypt.hash(password, 10);
     const newUser = new User({
         name: name,
