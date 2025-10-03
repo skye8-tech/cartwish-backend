15e84de51f77ccf850d445de444920b80d7f4e9d
Cheneba <chenebametuge14@gmail.com>
2025-10-03 10:30:12 +0100

    feat: setup endpoint to create a category Installed express-list-routes, that helps me to list all the routes present in the app Installed multer pkg that is used in file uploads

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ require("dotenv").config();
 require("./config/passport");
 const express = require("express");
 const mongoose = require("mongoose");
+const expressListRoutes = require("express-list-routes");
 const app = express();
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
+const categoryRoutes = require("./routes/category");
 
 mongoose
     .connect("mongodb://localhost:27017/cartwish")
-    .then(() => console.log("MongoDB Connected"))
+    .then(() => console.log("MongoDB Connected\n"))
     .catch((err) => console.log("MongoDB Connection Failed"));
 
 app.use(express.json());
@@ -17,6 +19,14 @@ app.use(express.urlencoded());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/category", categoryRoutes);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+app.listen(PORT, () => {
+    // List all endponts
+    console.log("All Available Endpoints");
+    expressListRoutes(authRoutes, { prefix: "/api/auth" });
+    expressListRoutes(userRoutes, { prefix: "/api/user" });
+    expressListRoutes(categoryRoutes, { prefix: "/api/category" });
+    console.log(`Server is listening on port ${PORT}...`);
+});
---
+++ routes/category.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const Category = require("../models/category");
+
+// Filters for image file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "upload/category");
+    },
+    filename: (req, file, cb) => {
+        const timestamp = Date.now();
+        const originalName = file.originalname
+            .replace(/\s+/g, "_")
+            .replace(/[^a-zA-Z0-9.-_]/g, "");
+        cb(null, `${timestamp}-${originalName}`);
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error("invalid file type. Only JPEG, PNG, and GIF are allowed"));
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 2 * 1024 * 1024,
+    },
+});
+
+router.post("/", upload.single("icon"), async (req, res) => {
+    if (!req.body.name || !req.file) {
+        return res.status(400).json({ message: "Name and icon are required" });
+    }
+
+    const newCategory = new Category({
+        name: req.body.name,
+        image: req.file.filename,
+    });
+
+    await newCategory.save();
+
+    res.status(201).json({
+        message: "Category added successfully",
+        category: newCategory,
+    });
+});
+
+module.exports = router;
