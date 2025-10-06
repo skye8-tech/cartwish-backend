51b76995456fbc2126c4a79d7c22f10f5d52b763
Cheneba <chenebametuge14@gmail.com>
2025-10-06 15:32:51 +0100

    install winston package for logging error messages to the console, file and db, it can be done to external services too

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 require("dotenv").config();
 require("./config/passport");
+require("winston-mongodb");
 const express = require("express");
 const mongoose = require("mongoose");
 const expressListRoutes = require("express-list-routes");
+const winston = require("winston");
 const app = express();
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/products");
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console({ level: "debug" }),
+        new winston.transports.File({
+            filename: "logs/errors.log",
+            level: "error",
+        }),
+        new winston.transports.MongoDB({
+            db: "mongodb://localhost:27017/cartwish",
+            level: "error",
+        }),
+    ],
+});
 
 mongoose
     .connect("mongodb://localhost:27017/cartwish")
@@ -25,6 +46,15 @@ app.use("/api/user", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 
+app.use((error, req, res, next) => {
+    logger.error(error.message, {
+        stack: error.stack,
+        method: req.method,
+        path: req.originalUrl,
+    });
+    return res.status(500).json({ message: "Internal Server Error!" });
+});
+
 const listAllRoutes = () => {
     console.log("All Available Endpoints");
     console.log("<----- auth endpoints ----->");
@@ -35,7 +65,6 @@ const listAllRoutes = () => {
     expressListRoutes(categoryRoutes, { prefix: "/api/category" });
     console.log("<----- products endpoints ----->");
     expressListRoutes(productRoutes, { prefix: "/api/product" });
-    console.log(`Server is listening on port ${PORT}...`);
 };
 
 const PORT = process.env.PORT || 8000;
@@ -43,4 +72,5 @@ const server = app.listen(PORT, () => {
     if (process.env.ROUTE_LIST == 1) {
         listAllRoutes();
     }
+    logger.info(`Server is listening on port ${PORT}...`);
 });
